f3af6680cea0fb73c7f3324fb27402d3663143c0
pancake~ <834430775@qq.com>
2023-02-01 22:09:29 +0800

    init uniapp project

---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<!-- 顶部导航栏 -->
+		<u-navbar
+			title=""
+			bgColor="40AE36"
+		>
+			<template #left>
+				<view>
+					左边
+				</view>
+			</template>
+			<template #right>
+				<view>
+					右边
+				</view>
+			</template>
+		</u-navbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			trigger(){
+				console.log('----waht here fucking ----')
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
